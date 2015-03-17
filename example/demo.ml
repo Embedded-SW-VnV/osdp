031cff0b31ea7d917eeb6065f5a27cdc22a1cc53
Pierre Roux <pierre.roux@onera.fr>
2015-03-17 09:00:34 +0000

    Example.

--- example/demo.ml
+++ example/demo.ml
@@ -2,7 +2,7 @@
 (* #use "topfind";; *)
 (* #require "osdp";; *)
 
-let solver = Osdp.Sdp.Csdp  (* Osdp.Sdp.Mosek *)
+let solver = Osdp.Sdp.Mosek  (* Osdp.Sdp.Mosek *)
 
 open Osdp.Lmi.Float
 
@@ -28,22 +28,17 @@ open Osdp.Sos.Float
 
 let () = Format.printf "SOS@."
 let deg = 4
-let p = { name = Osdp.Ident.create "p";
-          nb_vars = 2;
-          degree = deg;
-          homogeneous = true }
+let p = var_poly "p" 2 ~homogen:true deg
 let a0 = <:sos< 1.5 x0 - 0.7 x1 >>
 let a1 = <:sos< x0 >>
-let e1 = <:sos< ?p - ?p(a0, x0) >>
-(* or let l = [a0; a1] in let e1 = <:sos< ?p - ?p($l$) >> *)
-let e2 = <:sos< ?p - (x0^4 + x1^4) >>
+let e1 = <:sos< p - p(a0, x0) >>
+(* or let l = [a0; a1] in let e1 = <:sos< p - p($l$) >> *)
+let e2 = <:sos< p - (x0^4 + x1^4) >>
 let () = Format.printf "e1 = %a@." pp e1
 let () = Format.printf "e2 = %a@." pp e2
 let _, _, vars = solve ~solver Purefeas [e1; e2]
 let () =
   try
-    let p' = Osdp.Ident.Map.find p.name vars in
-    match p' with
-    | Scalar _ -> Format.printf "SDP error.@."
-    | Poly p' -> Format.printf "%a = %a@." Osdp.Ident.pp p.name Poly.pp p'
+    let p' = value_poly p vars in
+    Format.printf "%a = %a@." pp p Poly.pp p'
   with Not_found -> Format.printf "SDP error.@."
