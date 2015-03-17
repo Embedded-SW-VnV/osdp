8029c365e9c083c3bf0348ed6e7c7689cabb2470
Pierre Roux <pierre.roux@onera.fr>
2015-03-17 09:04:34 +0000

    Example.

--- example/demo.ml
+++ example/demo.ml
@@ -2,24 +2,22 @@
 (* #use "topfind";; *)
 (* #require "osdp";; *)
 
-let solver = Osdp.Sdp.Mosek  (* Osdp.Sdp.Mosek *)
+let solver = Osdp.Sdp.Csdp  (* Osdp.Sdp.Mosek *)
 
 open Osdp.Lmi.Float
 
 let () = Format.printf "LMI@."
-let p = Osdp.Ident.create "p"
+let p = var "p"
 let a = << [1.5, -0.7; 1, 0] >>  (* or <:lmi< ... >> *)
-let e1 = << ?p - a ' * ?p * a >>
-let e2 = << ?p - eye(2) >>
+let e1 = << p - a ' * p * a >>
+let e2 = << p - eye(2) >>
 let () = Format.printf "e1 = %a@." pp e1
 let () = Format.printf "e2 = %a@." pp e2
 let _, _, vars = solve ~solver Purefeas [e1; e2]
 let () =
   try
-    let r = Osdp.Ident.Map.find p vars in
-    match r with
-    | Scalar _ -> Format.printf "SDP error.@."
-    | Mat r -> Format.printf "%a = %a@." Osdp.Ident.pp p Mat.pp r
+    let p' = value_mat p vars in
+    Format.printf "%a = %a@." pp p Mat.pp p'
   with Not_found -> Format.printf "SDP error.@."
 
 let () = Format.printf "@."
