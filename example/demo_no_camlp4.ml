9069d1fa2377b6140aca6915d8c95406a634b249
Pierre Roux <pierre.roux@onera.fr>
2015-03-17 09:40:21 +0000

    Example without camlp4.

--- example/demo_no_camlp4.ml
+++ example/demo_no_camlp4.ml
@@ -16,21 +16,19 @@ open Osdp.Lmi.Float
 
 let () = Format.printf "LMI@."
 let a = Mat.of_list_list [[1.5; -0.7]; [1.; 0.]]
-let p_id = Osdp.Ident.create "p"
-let e1 = Sub (Var p_id,
+let p = var "p"
+let e1 = Sub (p,
               Mult (Mult (Transpose (Const a),
-                          Var p_id),
+                          p),
                     Const a))
-let e2 = Sub (Var p_id, Eye 2)
+let e2 = Sub (p, Eye 2)
 let () = Format.printf "e1 = %a@." pp e1
 let () = Format.printf "e2 = %a@." pp e2
 let _, _, vars = solve ~solver Purefeas [e1; e2]
 let () =
   try
-    let p = Osdp.Ident.Map.find p_id vars in
-    match p with
-    | Scalar _ -> Format.printf "SDP error.@."
-    | Mat p -> Format.printf "%a = %a@." Osdp.Ident.pp p_id Mat.pp p
+    let p' = value_mat p vars in
+    Format.printf "%a = %a@." pp p Mat.pp p'
   with Not_found -> Format.printf "SDP error.@."
 
 let () = Format.printf "@."
@@ -40,24 +38,18 @@ open Osdp.Sos.Float
 let () = Format.printf "SOS@."
 let deg = 4
 let names = ["x"; "y"]
-let pp = pp ~names
-let pp_poly = Poly.pp ~names
+let pp = pp_names names
 let pol_of_list l =
   Poly.of_list (List.map (fun (s, m) -> Osdp.Monomial.of_list m, s) l)
 let a = List.map pol_of_list [[1.5, [1]; -0.7, [0; 1]]; [1., [1]; 0., [0; 1]]]
-let p = { name = Osdp.Ident.create "p";
-          nb_vars = 2;
-          degree = deg;
-          homogeneous = true }
-let e1 = Sub (Var p, Compose (Var p, List.map (fun p -> Const p) a))
-let e2 = Sub (Var p, Const (pol_of_list [1., [deg]; 1., [0; deg]]))
+let p = var_poly "p" 2 ~homogen:true deg
+let e1 = Sub (p, Compose (p, List.map (fun p -> Const p) a))
+let e2 = Sub (p, Const (pol_of_list [1., [deg]; 1., [0; deg]]))
 let () = Format.printf "e1 = %a@." pp e1
 let () = Format.printf "e2 = %a@." pp e2
 let _, _, vars = solve ~solver Purefeas [e1; e2]
 let () =
   try
-    let p' = Osdp.Ident.Map.find p.name vars in
-    match p' with
-    | Scalar _ -> Format.printf "SDP error.@."
-    | Poly p' -> Format.printf "%a = %a@." Osdp.Ident.pp p.name pp_poly p'
+    let p' = value_poly p vars in
+    Format.printf "%a = %a@." pp p (Poly.pp_names names) p'
   with Not_found -> Format.printf "SDP error.@."
