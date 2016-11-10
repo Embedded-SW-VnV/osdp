45bb677fc7163fc1cbfe23d8be6c16b8844e72c4
Pierre Roux <pierre.roux@onera.fr>
2016-11-10 10:00:27 +0000

    Add example demo_sos3.

---
+++ example/demo_sos3.ml
@@ -0,0 +1,14 @@
+(* Does: x <= 1 -> x^2 + 1 >= 0 hold ?
+   Encoded as: x^2 + 1 - q (1 - x) SOS, q SOS *)
+
+let solver = Osdp.Sdp.Csdp
+
+module Sos = Osdp.Sos.Float
+               
+let _ =
+  let q, _ = Sos.var_poly "q" 1 2 in
+  let p = Sos.(??0**2 + !1.) in
+  let e = Sos.(p - q * (!1. - ??0)) in
+  let () = Format.printf "e = %a@." Sos.pp e in
+  let ret, _, _, _ = Sos.solve ~solver Sos.Purefeas [e; q] in
+  Format.printf "ret = %a@." Osdp.SdpRet.pp ret
